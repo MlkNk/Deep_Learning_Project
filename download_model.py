a758cd3162260b29fed786dfc3d5363694ea9394
MlkNk <malaika.nkuida@efrei.net>
2025-06-08 23:25:31 +0200

    Update download_model.py

--- download_model.py
+++ download_model.py
@@ -5,13 +5,12 @@ import zipfile
 def download_model():
     model_folder = "bert_model_final"
     if not os.path.exists(model_folder):
-        os.makedirs(model_folder, exist_ok=True)
         url = "https://drive.google.com/uc?id=1O5Z2moYiT0ji-YbviEXgVUEa0ahF9YFa"
         output = "bert_model_final.zip"
         gdown.download(url, output, quiet=False)
 
         with zipfile.ZipFile(output, 'r') as zip_ref:
-            zip_ref.extractall(model_folder)
+            zip_ref.extractall(".")  # Extrait les fichiers au bon endroit
         os.remove(output)
     else:
         print(f"Le dossier {model_folder} existe déjà.")
